383f4e8b624bd0bb34a8653c9bf2dec758b81c5e
Sarah Solarin <solarinsarah3@gmail.com>
2023-01-15 15:11:58 +0000

    first commit

---
+++ apps/auth-microservice/src/app/app.controller.ts
@@ -0,0 +1,15 @@
+import { CreateUserDto } from '@nestjs-microservices/shared/dto';
+import { Controller, ParseIntPipe, ValidationPipe } from '@nestjs/common';
+import { EventPattern, MessagePattern, Payload } from '@nestjs/microservices';
+
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @EventPattern('create_user')
+  handleUserCreate(@Payload(ValidationPipe) data: CreateUserDto) {
+    this.appService.createUser(data);
+  }
+}
---
+++ apps/auth-microservice/src/main.ts
@@ -0,0 +1,29 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app/app.module';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.KAFKA,
+      options: {
+        client: {
+          brokers: ['localhost:9092'],
+        },
+        consumer: {
+          groupId: 'auth-consumer',
+        },
+      },
+    }
+  );
+  await app.listen();
+}
+
+bootstrap();
